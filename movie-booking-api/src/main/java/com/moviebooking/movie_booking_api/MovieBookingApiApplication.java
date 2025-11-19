930abc7518ea212615ab3ee5a9f280f5acc73ade
hiep2004 <buituanhiepvinhtuong@gmail.com>
2025-11-19 15:54:24 +0700

    feat: add initial movie-booking-api

---
+++ movie-booking-api/src/main/java/com/moviebooking/movie_booking_api/MovieBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.moviebooking.movie_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingApiApplication.class, args);
+	}
+
+}
